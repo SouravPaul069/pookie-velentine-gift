5ce0c254308b638d53eb5d8a1e75dfa376676746
Sourav Paul <narayanchandrapaul467@gmail.com>
2026-02-14 20:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
 let highestZ = 1;
+let activePaper = null;
+
+document.addEventListener("mousemove", move);
+document.addEventListener("touchmove", move, { passive: false });
+
+window.addEventListener("mouseup", stopDrag);
+window.addEventListener("touchend", stopDrag);
+
+function move(e) {
+  if (!activePaper) return;
+
+  const x = e.touches ? e.touches[0].clientX : e.clientX;
+  const y = e.touches ? e.touches[0].clientY : e.clientY;
+
+  const dx = x - activePaper.prevX;
+  const dy = y - activePaper.prevY;
+
+  activePaper.currentX += dx;
+  activePaper.currentY += dy;
+
+  activePaper.prevX = x;
+  activePaper.prevY = y;
+
+  activePaper.el.style.transform =
+    `translate(${activePaper.currentX}px, ${activePaper.currentY}px) rotate(${activePaper.rotation}deg)`;
+}
+
+function stopDrag() {
+  activePaper = null;
+}
 
 class Paper {
-  holding = false;
-  prevX = 0;
-  prevY = 0;
-  currentX = 0;
-  currentY = 0;
-  rotation = Math.random() * 30 - 15;
+  constructor(el) {
+    this.el = el;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.currentX = 0;
+    this.currentY = 0;
+    this.rotation = Math.random() * 30 - 15;
 
-  init(paper) {
+    this.el.style.transform = `rotate(${this.rotation}deg)`;
 
-    const move = (x, y) => {
-      if (!this.holding) return;
+    this.init();
+  }
 
-      const dx = x - this.prevX;
-      const dy = y - this.prevY;
-
-      this.currentX += dx;
-      this.currentY += dy;
-
-      this.prevX = x;
-      this.prevY = y;
-
-      paper.style.transform =
-        `translate(${this.currentX}px, ${this.currentY}px) rotate(${this.rotation}deg)`;
-    };
-
-    // Mouse Move
-    document.addEventListener("mousemove", (e) => {
-      move(e.clientX, e.clientY);
-    });
-
-    // Touch Move
-    document.addEventListener("touchmove", (e) => {
-      move(e.touches[0].clientX, e.touches[0].clientY);
-    }, { passive: false });
-
-    // Mouse Down
-    paper.addEventListener("mousedown", (e) => {
-      this.holding = true;
-      paper.style.zIndex = highestZ++;
+  init() {
+    this.el.addEventListener("mousedown", (e) => {
+      activePaper = this;
+      this.el.style.zIndex = highestZ++;
       this.prevX = e.clientX;
       this.prevY = e.clientY;
     });
 
-    // Touch Start
-    paper.addEventListener("touchstart", (e) => {
-      this.holding = true;
-      paper.style.zIndex = highestZ++;
+    this.el.addEventListener("touchstart", (e) => {
+      activePaper = this;
+      this.el.style.zIndex = highestZ++;
       this.prevX = e.touches[0].clientX;
       this.prevY = e.touches[0].clientY;
-    });
-
-    window.addEventListener("mouseup", () => {
-      this.holding = false;
-    });
-
-    window.addEventListener("touchend", () => {
-      this.holding = false;
     });
   }
 }
 
 document.querySelectorAll(".paper").forEach(paper => {
-  new Paper().init(paper);
+  new Paper(paper);
 });
